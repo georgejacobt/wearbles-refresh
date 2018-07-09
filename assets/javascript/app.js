f10ae7297c277c36277a0d32ccd3e1b1afc3281d
George Jacob Work <36488006+georgejacobt@users.noreply.github.com>
2018-07-08 21:33:54 -0500

    working right slider and papaparse

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,14 +16,43 @@ var myFullpage = new fullpage('#fullpage', {
     css3:false
 });
 
-
+let bodyPart = "";
+let description = "";
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
-
+  var dataPath = "../WearableRefresh/assets/data/bhpEquipment1.csv";
+        Papa.parse(dataPath, {
+            download: true,
+            header: true,
+            complete: function(results) {
+            console.log("Finished:", results.data);
+            bodyPart = results.data[14].bodyPart;
+            description = results.data[14].description
+            console.log(results.data[14].bodyPart);
+            console.log(results.data[14].description);
+            // equipmentDB = TAFFY(results.data);
+            // headArray = equipmentDB({bodyPart:"head"}).get();
+            // bodyArray = equipmentDB({bodyPart:"body"}).get();
+            // handArray = equipmentDB({bodyPart:"hand"}).get();
+            // wristArray = equipmentDB({bodyPart:"wrist"}).get();
+            // waistArray = equipmentDB({bodyPart:"waist"}).get();
+            // headArrayLength = headArray.length;
+            // bodyArrayLength = bodyArray.length;
+            // handArrayLength = handArray.length;
+            // wristArrayLength = wristArray.length;
+            // waistArrayLength = waistArray.length;
+          }
+        })
     $("#vest-btn").on("click",function(){
         $("#vest").attr("src","../WearableRefresh/assets/images/body.png");
         $("#vest-btn").attr("class","");
         $("#rightNav").attr("class","rightside");
+        $("#bodyPart").text(bodyPart);
+        $("#description").text(description);
+
+
+        
+      
     })
   });
 
